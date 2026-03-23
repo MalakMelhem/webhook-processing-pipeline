05e6691b371e7e43b7b7ed3dfc4328619fc94100
MalakMelhem <malak.melhem98@gmail.com>
2026-03-24 01:11:20 +0200

    implement CRUD operations for pipelines with PostgreSQL integration

---
+++ src/db/queries.ts
@@ -0,0 +1,31 @@
+import { pool } from "./index.js";
+
+export async function createPipeline(pipelineName:string, webhookUrl:string){
+    const createdTime = new Date().toISOString(); 
+    const query ='INSERT INTO pipelines (pipeline_name, webhook_url, created_time) VALUES ($1,$2,$3) RETURNING * ;';
+    const values =[pipelineName,webhookUrl,createdTime];
+    const result=await pool.query(query,values);
+    return result.rows[0];
+}
+export async function createSubscriber(pipelineId:string, subscriberUrl:string){
+    const createdTime = new Date().toISOString(); 
+    const query ='INSERT INTO subscribers (pipeline_id, subscriber_url, created_time) VALUES ($1,$2,$3)';
+    const values =[pipelineId,subscriberUrl,createdTime];
+    const result=await pool.query(query,values);
+}
+export async function getPipleline(name: string) {
+    const query = 'SELECT * FROM pipelines WHERE pipeline_name = $1';
+    const result=await pool.query(query,[name]);
+    return result.rows[0];
+}
+export async function updatePipleline(id: string, pipelineName: string, webhookUrl: string){
+    const query = 'UPDATE pipelines SET pipeline_name = $1, webhook_url = $2 WHERE id = $3 RETURNING * ;';
+    const values = [pipelineName, webhookUrl, id];
+    const result=await pool.query(query,values);
+    return result.rows[0];
+}
+export async function deletePipleline(id: string){
+   const query = 'DELETE FROM pipelines WHERE id = $1 RETURNING *; ';
+   const result=await pool.query(query,[id]);
+   return result.rows[0];
+}
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,83 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
+import { getPipleline, createPipeline, updatePipleline, deletePipleline } from "./db/queries.js";
 import "dotenv/config";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.static("."));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Webhook pipeline running");
+});
+
+app.post("/pipelines",async(req: Request,res: Response)=>{
+  const {pipelineName, webhookUrl} = req.body;
+
+if (!pipelineName ||!webhookUrl) {
+  return res.status(400).json({ error: "Missing data" });
+}
+try{
+
+const existing = await getPipleline(pipelineName);
+  if (existing) {
+    return res.status(409).json({ error: "Pipeline already exists" });
+  }
+const newPipeline=await createPipeline(pipelineName, webhookUrl);
+res.status(201).json(newPipeline);
+}catch(err){
+  console.error(err);
+  res.status(500).json({ error: "Server error" });
+}
+
+});
+app.get("/pipelines/:pipelineName", async(req: Request,res: Response)=>{
+  const { pipelineName }= req.params;
+try{
+ const pipeline= await getPipleline(pipelineName as string);
+ if (!pipeline) {
+    return res.status(404).json({ error: "Pipeline not found" });
+  }
+
+  res.status(200).json(pipeline);
+}catch(err){
+  console.error(err);
+  res.status(500).json({ error: "Server error" });
+}
+
+});
+app.put("/pipelines/:id", async(req: Request,res: Response)=>{
+  const { id }= req.params;
+  const {pipelineName, webhookUrl}=req.body;
+
+  if (!pipelineName || !webhookUrl) {
+   return res.status(400).json({ error: "Missing data" });
+  }
+try{
+  const updated= await updatePipleline(id as string, pipelineName ,webhookUrl);
+  if(!updated){
+     return res.status(404).json({ error: "Pipeline not found" });
+  }
+  res.status(200).json(updated);
+}catch(err){
+  console.error(err);
+  res.status(500).json({ error: "Server error" });
+}
+});
+app.delete("/pipelines/:id", async(req: Request,res: Response)=>{
+  const { id }= req.params;
+try{
+  const deleted= await deletePipleline(id as string);  
+  if(!deleted){
+     return res.status(404).json({ error: "Pipeline not found" });
+  }
+  res.status(200).json(deleted);
+}catch(err){
+   console.error(err);
+  res.status(500).json({ error: "Server error" });
+}
 });
 
 app.listen(PORT, () => {
